9f8fb90150e12c4fabf11971906e9cc542851c4f
rapicastillo <rapi.castillo@gmail.com>
2017-05-31 00:34:17 -0400

    Header area

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -10,11 +10,20 @@ import TypeMenu from 'components/TypeMenu';
 import styled from 'styled-components';
 import ModalMenu from 'components/ModalMenu';
 import Link from 'components/Link';
-
+import LogoImageEn from 'assets/images/logo-en.png';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  position: absolute;
+  top: 10px;
+  left: 50%;
+  margin: 0;
+  transform: translate(-540px, 0);
+`;
+const Viewport = styled.div`
+  position: relative;
+`;
 const PageHeader = styled.header`
   position: fixed;
   width: 100%;
@@ -29,10 +38,12 @@ const PageHeader = styled.header`
 function Header() {
   return (
     <PageHeader>
-      <Title>
-        <Link to='/'><FormattedMessage {...messages.header} /></Link>
-      </Title>
-      <ModalMenu />
+      <Viewport>
+        <Title>
+          <Link to='/'><img src={LogoImageEn} /></Link>
+        </Title>
+        <ModalMenu />
+      </Viewport>
     </PageHeader>
   );
 }
