63f728dd6867b1e4b11c60fd99f5bafb0b8a13c3
rapicastillo <rapi.castillo@gmail.com>
2017-06-22 14:03:16 -0400

    Type tile on flag hover

--- app/components/TypeFlagTooltip/index.js
+++ app/components/TypeFlagTooltip/index.js
@@ -8,7 +8,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import messages from 'containers/HomePage/messages';
 
 const Container = styled.div`
   position: absolute;
@@ -21,18 +21,42 @@ const Container = styled.div`
   max-width: 500px;
 `;
 
+const TypeTile = styled.span`
+  display: inline-block;
+  background-image: url(${props=>require('assets/images/type-tile/' + props.type + '.png')});
+  background-size: cover;
+  width: 40px;
+  height: 40px;
+`;
+
+const TypeName = styled.div`
+  font-family: 'Avenir', 'Kaff Bold';
+  font-size: 10px;
+  text-transform: capitalize;
+  text-decoration: underline;
+  margin-top: -10px;
+`;
+
+
+
 const Viewport = styled.div``;
 const List = styled.ul`margin: 0; padding: 0`;
 const Item = styled.li`display: inline-block;
-
-
+  text-align: center;
+  position: relative;
+  white-space: nowrap;
+  padding-right: 10px;
   &::after {
+    position: absolute;
+    top: 10px;
+    right: -3px;
     content: '+';
     display: inline-block;
     padding: 0px 3px;
   }
 
   &:last-child {
+    padding-right: 0px;
     &::after { content: ''; }
   }
 `;
@@ -42,7 +66,13 @@ function TypeFlagTooltip(props) {
     <Container show={props.show}>
       <Viewport>
         <List>
-          {props.truths.map((item, ind) => !item.show ? null : (<Item key={ind}>{item.name}</Item>))}
+          {props.truths.map((item, ind) =>  !item.show ? null : (
+            <Item key={ind}>
+              <TypeTile type={item.name} />
+              <TypeName>
+                <FormattedMessage {...messages[item.name]} />
+              </TypeName>
+            </Item>))}
         </List>
       </Viewport>
     </Container>
