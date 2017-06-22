6e4792f4f7fc5c69429e5ed2bdabedcf0029bfc7
rapicastillo <rapi.castillo@gmail.com>
2017-06-22 13:20:20 -0400

    fix border line for ar and enes

--- app/containers/Tools/ToolsArea.js
+++ app/containers/Tools/ToolsArea.js
@@ -116,11 +116,10 @@ export class ToolsArea extends React.PureComponent { // eslint-disable-line reac
   }
 
   render() {
-
+    const lang=this.props.intl.locale;
     return (
       <ToolsListContainer show={this.props.show} lang={this.props.lang} rotate={true}>
-
-        <ToolsListMenu show={this.props.Tools.viewType === NEWS_FEED}>
+        <ToolsListMenu lang={lang} show={this.props.Tools.viewType === NEWS_FEED}>
           <ToolsListMenuItem>
             <ToolsButton toShow={this.state.newsFeed === TWITTER_FEED} onClick={()=>this.handleNewsFeedChange(TWITTER_FEED)}>
               <Isvg src={TwitterIcon} />
@@ -133,7 +132,7 @@ export class ToolsArea extends React.PureComponent { // eslint-disable-line reac
           </ToolsListMenuItem>
         </ToolsListMenu>
 
-        <ToolsListMenu show={this.props.Tools.viewType === MY_TOOLS}>
+        <ToolsListMenu lang={lang} show={this.props.Tools.viewType === MY_TOOLS}>
           <ToolsListMenuItem>
             <ToolsButton
               onClick={()=>this.handleClick(DOWNDLOAD_PDF)}
