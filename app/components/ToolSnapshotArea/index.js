2874ed74282bce38eabea775506d4c8224a7ec52
rapicastillo <rapi.castillo@gmail.com>
2017-06-30 22:20:13 -0400

    Important font-size

--- app/components/ToolSnapshotArea/index.js
+++ app/components/ToolSnapshotArea/index.js
@@ -49,7 +49,7 @@ const SnapshotCallout = styled.div`
 `;
 const Link = styled.a`
     color: #838486;
-    font-size: ${props=>props.lang === 'ar' ? '16px' : '18px'};
+    font-size: ${props=>props.lang === 'ar' ? '16px !important' : '18px !important'};
     text-transform: uppercase;
     font-weight: bold;
     ${props=>props.lang === 'ar' ? 'text-align: right;' : ''}
