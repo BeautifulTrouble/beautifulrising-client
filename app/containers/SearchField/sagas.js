ed9bea93dcf05b41534906d52c5ace82e81aca80
rapicastillo <rapi.castillo@gmail.com>
2017-06-30 21:11:33 -0400

    Fix issue on search

--- app/containers/SearchField/sagas.js
+++ app/containers/SearchField/sagas.js
@@ -5,15 +5,12 @@ import { LIST_VIEW } from 'containers/ToolsViewOptions/constants';
 import { SEARCH_FIELD_CHANGED } from './constants';
 // automatically switch to list view when searching
 export function* switchToListView() {
-
-
-  yield put(changeToolView(LIST_VIEW));
+  // yield put(changeToolView(LIST_VIEW));
 }
 // Individual exports for testing
 export function* listenForSearch() {
   //IF a user searches, switch to list
-
-  yield takeLatest(SEARCH_FIELD_CHANGED, switchToListView);
+  // yield takeLatest(SEARCH_FIELD_CHANGED, switchToListView);
 }
 
 // All sagas to be loaded
