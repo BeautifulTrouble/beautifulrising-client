681e628bd74d0c459d9318632b8ff5ad285d267a
rapicastillo <rapi.castillo@gmail.com>
2017-07-02 08:34:43 -0400

    Testing

--- app/components/CommonComponents/constants.js
+++ app/components/CommonComponents/constants.js
@@ -3,4 +3,5 @@ export const TYPE_TACTICS = 'tactic';
 export const TYPE_PRINCIPLES = 'principle';
 export const TYPE_THEORIES = 'theory';
 export const TYPE_METHODOLOGIES = 'methodology';
+//export const RECAPTCHA_SITE_KEY = '6LfeoicUAAAAADHJTSBd6PhfWyrJ1O_5f2Lx5GMe';
 export const RECAPTCHA_SITE_KEY = '6LeCpCgTAAAAAFc4TwetXb1yBzJvaYo-FvrQvAlx';
