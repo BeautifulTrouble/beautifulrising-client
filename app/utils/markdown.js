08a37b609d2d8d1709b21be037244f48d53a3284
rapicastillo <rapi.castillo@gmail.com>
2017-07-07 10:06:55 -0400

    Caption markdowns to open in new window

--- app/utils/markdown.js
+++ app/utils/markdown.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 export function RouterLink(props) {
   return (
     props.href.match(/^(https?:)?\/\//)
-      ? <a href={props.href}>{props.children}</a>
+      ? <a href={props.href} target={'_blank'}>{props.children}</a>
       : <Link to={props.href}>{props.children}</Link>
   );
 }
