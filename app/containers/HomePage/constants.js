30f7542449f2ce51e96389104d533a94c63a1ae0
rapicastillo <rapi.castillo@gmail.com>
2017-07-11 08:31:53 -0400

    Show snapshots in search

--- app/containers/HomePage/constants.js
+++ app/containers/HomePage/constants.js
@@ -8,3 +8,6 @@ export const DEFAULT_ACTION = 'app/HomePage/DEFAULT_ACTION';
 export const TAG_FILTER = 'tag';
 export const TYPE_FILTER = 'type';
 export const SEARCH_FILTER = 'search';
+export const MODULE_TYPE_FULL = 'full';
+export const MODULE_TYPE_GALLERY = 'gallery';
+export const MODULE_TYPE_UNTRANSLATED = 'untranslated';
