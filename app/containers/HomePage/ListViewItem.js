f37510d153c6b1fe43626a374c33030d43838104
rapicastillo <rapi.castillo@gmail.com>
2017-07-23 19:50:57 -0400

    List View Items refactor

--- app/containers/HomePage/ListViewItem.js
+++ app/containers/HomePage/ListViewItem.js
@@ -7,12 +7,14 @@
 import React, {PropTypes} from 'react';
 import styled from 'styled-components';
 import LanguageThemeProvider from 'components/LanguageThemeProvider';
-import { FormattedMessage, injectIntl } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import Link from 'components/Link';
 import AdderRemover from 'containers/Tools/AdderRemover';
-import messages from './messages';
 import {ToolType, ToolTitle, ListContainer, ListViewport, ListSpiel} from 'components/ToolsComponents';
 import { BR_IMAGE_PREFIX } from 'containers/Tools/constants';
+
+import TranslatableStaticText from 'containers/TranslatableStaticText';
+import staticText from './staticText';
 
 class ListViewItem extends React.Component {
 
@@ -25,7 +27,7 @@ class ListViewItem extends React.Component {
           <LanguageThemeProvider>
             <ToolType type={this.props.type}>
               <ToolType type={this.props.type}>
-                <FormattedMessage { ...messages[this.props.type] } />
+                <TranslatableStaticText { ...staticText[this.props.type] } />
               </ToolType>
             </ToolType>
             <ToolTitle><Link to={`/tool/${this.props.slug}`}>{this.props.title}</Link></ToolTitle>
