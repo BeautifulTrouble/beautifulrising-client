e5a0a25e0b955356de9c5570add72c1fc6250b54
rapicastillo <rapi.castillo@gmail.com>
2017-07-23 01:20:29 -0400

    Refactor BTAA area in AboutPage

---
+++ app/components/AboutPage/BTAAColumn.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+export default styled.td`
+  padding: 10px;
+  width: 50%;
+  vertical-align: top;
+`;
--- app/containers/AboutPage/BeautifulTroubleAA.js
+++ app/containers/AboutPage/BeautifulTroubleAA.js
@@ -18,16 +18,10 @@ import messages from './messages';
 import ActionAidLogo from 'assets/images/about/aa-logo.png';
 import BeautifulTroubleLogo from 'assets/images/about/bt-logo.png';
 
-const Container = styled.div``;
-const TableMenu = styled.table`border: 0;`;
-const Row = styled.tr``;
-const Column = styled.td`
-  padding: 10px;
-  width: 50%;
-  vertical-align: top;
-`;
+import BTAATableMenu from 'components/AboutPage/BTAATableMenu';
+import BTAARow from 'components/AboutPage/BTAARow';
+import BTAAColumn from 'components/AboutPage/BTAAColumn';
 
-const Image = styled.img``;
 
 export class BeautifulTroubleAA extends React.Component {
   renderHeader() {
@@ -46,27 +40,27 @@ export class BeautifulTroubleAA extends React.Component {
       <LanguageThemeProvider>
         <AboutSection id='beautiful-trouble-and-action-aid' lang={locale}>
           { this.props.hideHeader ? null : this.renderHeader() }
-          <Container>
+          <div>
             <ContentBlock>
-              <TableMenu>
-                <Row>
-                  <Column colSpan={"2"} style={{ padding: '0 25%', textAlign: 'center'}}>
+              <BTAATableMenu>
+                <BTAARow>
+                  <BTAAColumn colSpan={"2"} style={{ padding: '0 25%', textAlign: 'center'}}>
                     <Markdown source={this.props.allData.getIn(['about', 'beautiful-trouble-and-action-aid', 'introduction'])} />
-                  </Column>
-                </Row>
-                <Row>
-                  <Column>
+                  </BTAAColumn>
+                </BTAARow>
+                <BTAARow>
+                  <BTAAColumn>
                     <Markdown source={this.props.allData.getIn(['about', 'beautiful-trouble-and-action-aid', 'bt'])} />
-                    <Image src={BeautifulTroubleLogo} />
-                  </Column>
-                  <Column>
+                    <img src={BeautifulTroubleLogo} />
+                  </BTAAColumn>
+                  <BTAAColumn>
                     <Markdown source={this.props.allData.getIn(['about', 'beautiful-trouble-and-action-aid', 'aa'])} />
-                    <Image src={ActionAidLogo} />
-                  </Column>
-                </Row>
-              </TableMenu>
+                    <img src={ActionAidLogo} />
+                  </BTAAColumn>
+                </BTAARow>
+              </BTAATableMenu>
             </ContentBlock>
-          </Container>
+          </div>
         </AboutSection>
       </LanguageThemeProvider>
     );
