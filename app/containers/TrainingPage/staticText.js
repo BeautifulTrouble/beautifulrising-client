9fe378415e8a189e630ae2f6f9fc48d24646559a
rapicastillo <rapi.castillo@gmail.com>
2017-07-23 21:26:43 -0400

    Training Page translated anew

---
+++ app/containers/TrainingPage/staticText.js
@@ -0,0 +1,10 @@
+export default {
+  header: {
+    id: 'menu.training-and-resources',
+    defaultMessage: 'TRAINING + RESOURCES',
+  },
+  otherResources: {
+    id: 'menu.other',
+    defaultMessage: 'OTHER RESOURCES'
+  }
+};
