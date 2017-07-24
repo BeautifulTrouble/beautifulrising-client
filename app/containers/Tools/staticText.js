5243c862e75b947a238be054f024c6a8ee8649d7
rapicastillo <rapi.castillo@gmail.com>
2017-07-23 21:08:18 -0400

    Update Tools to change translation source

--- app/containers/Tools/staticText.js
+++ app/containers/Tools/staticText.js
@@ -6,5 +6,33 @@ export default {
   shareTool: {
     id: "sidebar.share",
     defaultMessage: "Share"
+  },
+  newsFeed: {
+    id: 'sidebar.news',
+    defaultMessage: 'News Feed',
+  },
+  myTools: {
+    id: 'sidebar.tools',
+    defaultMessage: 'My Tools',
+  },
+  remove: {
+    id: 'sidebar.remove',
+    defaultMessage: 'Remove'
+  },
+  share: {
+    id: 'sidebar.share',
+    defaultMessage: 'share'
+  },
+  pdfSpiel: {
+    id: 'app.containers.Tools.pdfSpiel',
+    defaultMessage: 'Click the button below to download a PDF version of your chosen tools'
+  },
+  pdfHeader: {
+    id: 'sidebar.download-heading',
+    defaultMessage: 'Download PDF'
+  },
+  pdfCTA: {
+    id: 'sidebar.download-link',
+    defaultMessage: 'Download PDF'
   }
 }
