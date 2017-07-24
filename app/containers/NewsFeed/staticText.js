f8ef8b5ec365c73fdb6c65315a69ee4a076e17b0
rapicastillo <rapi.castillo@gmail.com>
2017-07-23 20:00:30 -0400

    News Feed refactor and retranslating

---
+++ app/containers/NewsFeed/staticText.js
@@ -0,0 +1,10 @@
+export default {
+  header: {
+    id: 'sidebar.news-intro',
+    defaultMessage: 'We pull various newsworthy hashtags associated with different campaigns and movements to keep you in the loop with what’s happening in the world of activism!',
+  },
+  follow: {
+    id: 'sidebar.follow',
+    defaultMessage: 'Follow'
+  }
+};
