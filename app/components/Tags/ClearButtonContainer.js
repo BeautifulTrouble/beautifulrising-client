2bc16436b4bcaf98834f78b97862db0ed7435097
rapicastillo <rapi.castillo@gmail.com>
2017-07-28 18:06:29 -0400

    Fixed issues with Arabic language -- https://3.basecamp.com/3161453/buckets/3412210/todos/610278593

--- app/components/Tags/ClearButtonContainer.js
+++ app/components/Tags/ClearButtonContainer.js
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 export default styled.div`
   position: absolute;
   top: 0;
-  left: -15px;
+  ${p=>p.theme.isArabic ? 'right': 'left'}: -15px;
   text-align: center;
   color: #828486;
   button {
+    cursor: pointer;
     outline: none;
     padding-right: 0;
     padding-left: 0;
--- app/containers/HomePage/ToolList.js
+++ app/containers/HomePage/ToolList.js
@@ -8,6 +8,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default styled.div`
+  margin-top: 36px;
   &::after {
     content: ' ';
     clear: both;
