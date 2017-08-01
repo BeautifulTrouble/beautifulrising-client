a73be3e8c93913922a38b3d14a66314b2ffd949d
rapicastillo <rapi.castillo@gmail.com>
2017-07-31 23:58:31 -0400

    Fixing minor issues in ToolPAge

--- app/containers/ToolPage/MainStage/ShortContent.js
+++ app/containers/ToolPage/MainStage/ShortContent.js
@@ -4,17 +4,19 @@ import LanguageThemeProvider from 'components/LanguageThemeProvider';
 import ContentBlock from 'components/ContentBlock';
 import { RouterLink } from 'utils/markdown';
 import { PROP_SHORT_WRITE_UP } from '../constants';
-
+import {ShortContentContainer} from 'components/ToolPage/Main'
 
 export default function(props) {
   if (!props[PROP_SHORT_WRITE_UP] ) return null;
   return(
     <LanguageThemeProvider>
       <ContentBlock>
-        <Markdown
-          source={props[PROP_SHORT_WRITE_UP].replace(/\(([^()]*?)\.jpg\)/g,"(https://www.beautifulrising.org/$1.jpg)") }
-          renderers={{Link: RouterLink}}
-        />
+        <ShortContentContainer>
+          <Markdown
+            source={props[PROP_SHORT_WRITE_UP].replace(/\(([^()]*?)\.jpg\)/g,"(https://www.beautifulrising.org/$1.jpg)") }
+            renderers={{Link: RouterLink}}
+          />
+        </ShortContentContainer>
       </ContentBlock>
     </LanguageThemeProvider>
   );
