e2cc28c174f91e122f2762aa99fae1aec68c8544
rapicastillo <rapi.castillo@gmail.com>
2017-07-31 22:47:27 -0400

    Set whats happening reducers

--- app/containers/WhatsHappening/constants.js
+++ app/containers/WhatsHappening/constants.js
@@ -5,3 +5,6 @@
  */
 
 export const DEFAULT_ACTION = 'app/WhatsHappening/DEFAULT_ACTION';
+export const LOAD_WHATSHAPPENING = 'app/WhatsHappening/DEFAULT_ACTION';
+export const LOADING_WHATSHAPPENING_COMPLETE = 'app/WhatsHappening/LOADING_WHATSHAPPENING_COMPLETE';
+export const LOADING_WHATSHAPPENING_ERROR = 'app/WhatsHappening/LOADING_WHATSHAPPENING_ERROR';
--- app/containers/WhatsHappening/reducer.js
+++ app/containers/WhatsHappening/reducer.js
@@ -7,16 +7,42 @@
 import { fromJS } from 'immutable';
 import {
   DEFAULT_ACTION,
+  LOAD_WHATSHAPPENING,
+  LOADING_WHATSHAPPENING_COMPLETE,
+  LOADING_WHATSHAPPENING_ERROR
 } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  data: null,
+  loading: false,
+  complete: false,
+  error: false
+});
 
 function whatsHappeningReducer(state = initialState, action) {
   switch (action.type) {
     case DEFAULT_ACTION:
       return state;
-    default:
-      return state;
+
+    case LOAD_WHATSHAPPENING:
+
+      return state
+              .set('loading', true)
+              .set('complete', false)
+              .set('error', false);
+    case LOADING_WHATSHAPPENING_COMPLETE:
+
+      return state.set('data', action.data)
+                .set('complete', true)
+                .set('loading', false)
+                .set('error', false)
+
+    case LOADING_WHATSHAPPENING_ERROR:
+
+      return state.set('data', null)
+                .set('loading', false)
+                .set('complete', false)
+                .set('error', action.error);
   }
 }
 
