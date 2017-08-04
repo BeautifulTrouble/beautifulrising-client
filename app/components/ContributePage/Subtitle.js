a4597fd0549f105a0ca9e1738df22333ed904f7b
rapicastillo <rapi.castillo@gmail.com>
2017-08-04 18:03:22 -0400

    Please add 24px above the vertical line and 36 px below it https://3.basecamp.com/3161453/buckets/3412210/todos/620963618

--- app/components/ContributePage/Subtitle.js
+++ app/components/ContributePage/Subtitle.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 export default styled(SmallHeaderBlock)`
   border-bottom: 2px solid;
   padding-bottom: 12px;
-  margin-top: 70px;
   margin-bottom: 40px;
   margin-left: 30px;
   margin-right: 30px;
