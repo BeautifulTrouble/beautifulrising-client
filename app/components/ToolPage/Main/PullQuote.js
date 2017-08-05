efae504e13700ab6e2de903c1cd7f0eac3223fc1
rapicastillo <rapi.castillo@gmail.com>
2017-08-04 20:49:37 -0400

    Remove the horizontal line above a pull quote and please don't indent it https://3.basecamp.com/3161453/buckets/3412210/todos/614648987

--- app/components/ToolPage/Main/PullQuote.js
+++ app/components/ToolPage/Main/PullQuote.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export default styled.blockquote`
   float: ${p=>p.theme.isArabic?'right':'left'};
   padding: 0;
-  margin: 10px 0 10px 0;
-  margin-${p=>p.ar?'left':'right'}: 20px;
+  margin: 20px 0;
+
   line-height: 30px;
   max-width: 230px;
   width: 230px;
@@ -18,15 +18,5 @@ export default styled.blockquote`
   position: relative;
 
   font-size: 20px;
-  margin-left: 20px;
 
-  &::before {
-    content: ' ';
-    width: 42px;
-    height: 0;
-    border-bottom: 2px solid;
-    position: absolute;
-    top: 0;
-    ${p=>p.theme.isArabic?'right':'left'}: 0;
-  }
 `;
