28faf09978966db309b0b5e7881237990502ecab
rapicastillo <rapi.castillo@gmail.com>
2017-10-23 18:55:41 -0400

    Revert back to Local API

--- app/components/CommonComponents/constants.js
+++ app/components/CommonComponents/constants.js
@@ -5,10 +5,10 @@ export const TYPE_THEORIES = 'theory';
 export const TYPE_METHODOLOGIES = 'methodology';
 
 // RECAPTCHA FOR LOCALHOST
-export const RECAPTCHA_SITE_KEY = '6LfeoicUAAAAADHJTSBd6PhfWyrJ1O_5f2Lx5GMe';
+// export const RECAPTCHA_SITE_KEY = '6LfeoicUAAAAADHJTSBd6PhfWyrJ1O_5f2Lx5GMe';
 
 // RECAPTCHA FOR BETA AND PROD
-// export const RECAPTCHA_SITE_KEY = '6LeCpCgTAAAAAFc4TwetXb1yBzJvaYo-FvrQvAlx';
+export const RECAPTCHA_SITE_KEY = '6LeCpCgTAAAAAFc4TwetXb1yBzJvaYo-FvrQvAlx';
 
 export const PRODUCTION_ENDPOINT = 'https://api.beautifulrising.org/api/v1/all';
 export const DEVELOPMENT_ENDPOINT = 'https://api-develop.beautifulrising.org/api/v1/all';
